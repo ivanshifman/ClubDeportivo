ab46a8c5de7efb50f5f09d06570d6894c355ec78
Angel Sabato <19angel.s89@gmail.com>
2025-09-30 21:34:24 -0300

    "Fecha de nacimiento" se solapaba con el campo

--- ClubDeportivo/Controladores/FormRegistroSocio/FormRegistroSocio.cs
+++ ClubDeportivo/Controladores/FormRegistroSocio/FormRegistroSocio.cs
@@ -89,7 +89,10 @@ namespace ClubDeportivo.Controladores.FormRegistroSocio
                 throw new ArgumentException("Debe completar la ficha médica para poder registrarse.");
         }
 
-        
+        private void lblFechaNacimientoSocio_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
 
